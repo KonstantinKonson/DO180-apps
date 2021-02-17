ea09980535aa5ae439f05364873b9b54c368285d
Student User <student@workstation.lab.example.com>
2021-02-17 18:25:45 -0500

    updated app solomon

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "The time to gather stones\n";
+print "The time to through stones\n";
+print "The time to gather stones\n
 ?>
